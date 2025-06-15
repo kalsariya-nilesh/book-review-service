61aecb4a89770cd7ab71d3d16e064cb81eccd73e
Nilesh Kalsariya <kalsariyanilesh302@gmail.com>
2025-06-15 12:24:25 +0530

    Remove commented apollo-graphql code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,6 @@
-// import { ApolloServer } from '@apollo/server';
-// import { startStandaloneServer } from '@apollo/server/standalone';
 import { createYoga } from 'graphql-yoga';
 import { createServer } from 'http'
 import { schema } from './schema';
-
-// const server = new ApolloServer({ typeDefs, resolvers });
-
-// startStandaloneServer(server, { listen: { port: 4000 } })
-//   .then(({ url }) => console.log(`🚀 Server ready at ${url}`));
 
 const yoga = createYoga({schema});
 
