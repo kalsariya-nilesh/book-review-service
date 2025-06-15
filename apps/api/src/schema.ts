1097dba0b07a403a30486aca5660301c155704a8
Nilesh Kalsariya <kalsariyanilesh302@gmail.com>
2025-06-15 14:53:21 +0530

    added seed data for books

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,13 +1,45 @@
+import { books } from '@db/seed';
+
 import { createSchema } from 'graphql-yoga'
+
+type Review = {
+  bookId: string;
+  review: string;
+}
+
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
+    type Book {
+      id: ID!
+      title: String!
+      author: String!
+      reviews: [String!]!
+    }
+
+    input ReviewInput {
+      content: String!
+    }
+
     type Query {
-      getBooks: String
+      getBooks: [Book!]!
+    }
+
+    type Mutation {
+      addReview(bookId: ID!, review: ReviewInput!): Book
     }
   `,
   resolvers: {
     Query: {
-      getBooks: () => 'My Books!',
+      getBooks: () => books,
+    },
+    Mutation: {
+      addReview: async (_: any, { bookId, review }: any) => {
+        const book = books.find(b => b.id === bookId);
+        if (!book) throw new Error('Book not found');
+  
+        book.reviews.push(review.content);
+        return book;
+      },
     },
   },
 })
